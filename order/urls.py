f94db62d714097d54745211a2f54a8090cf38649
ostap <oneostap@gmail.com>
2018-04-04 23:41:24 +0300

    off csrf 3

--- order/urls.py
+++ order/urls.py
@@ -3,7 +3,7 @@ from .views import OrderViewSet
 from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
-	path('', OrderViewSet.as_view({'post':'post'}), name= 'Order'),
+	path('', csrf_exempt(OrderViewSet.as_view({'post':'post'})), name= 'Order'),
 	path('order-callback/', csrf_exempt(OrderViewSet.as_view({'post' : 'callback'})), name= 'Order callback'),
 	path('test/', OrderViewSet.as_view({'get':'get'}), name= 'Order Template')
 ]
