2aa614c9c10e4d9698855343ec9766580b96ffa5
Ostap Hyba <oneostap@gmail.com>
2018-04-05 10:44:06 +0300

    change admin order

--- order/admin.py
+++ order/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from .models import Order
 
 class OrderAdmin(admin.ModelAdmin):
+
+	fields = ('address', 'total', 'is_payed', 'type_of_payment')
+
 	class Meta:
 		model = Order
 
