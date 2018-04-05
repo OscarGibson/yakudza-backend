883a2be26d75fe4ef61adab95cbeecf8ca6955c6
Ostap Hyba <oneostap@gmail.com>
2018-04-05 10:59:20 +0300

    change admin order 5

--- order/admin.py
+++ order/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from .models import Order
 
-def type_of_payment(obj):
+def type_of_payment_str(obj):
     return ("%s" % ("Картка" if obj.type_of_payment == 1 else "Готівка")).upper()
-upper_case_name.short_description = 'Name'
+type_of_payment_str.short_description = 'Тип оплати'
 
 class OrderAdmin(admin.ModelAdmin):
 
-	list_display = ('address', 'total', 'is_payed', 'type_of_payment')
+	list_display = ('address', 'total', 'is_payed', 'type_of_payment_str')
 	fields = ('address', 'count', 'total', 'is_payed', 'type_of_payment', 'address', 'phone', 'comment')
 
 	class Meta:
