d96bdff56ebfcf970ab37be2d248a473d7759169
ostap <oneostap@gmail.com>
2018-04-11 22:26:38 +0300

    add invisible category

--- category/models.py
+++ category/models.py
@@ -4,6 +4,7 @@ class Category(models.Model):
 	""" Model for category """
 	name = models.CharField(max_length= 256, unique= True)
 	slug = models.CharField(max_length= 256)
+	is_show = models.BooleanField("Відображати", default= True)
 	created_at = models.DateTimeField(auto_now_add= True)
 	updated_at = models.DateTimeField(auto_now= True)
 
