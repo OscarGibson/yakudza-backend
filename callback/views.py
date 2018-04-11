aa1924d120e9e0be46b6f562ff4f19776c616760
ostap <oneostap@gmail.com>
2018-04-12 01:28:40 +0300

    add callback email send

--- callback/views.py
+++ callback/views.py
@@ -3,6 +3,12 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from .models import CallBack
 
+from django.conf import settings
+
+from django.core.mail import send_mail
+
+from django.template.loader import render_to_string
+
 
 class CallBackViewSet(ViewSet):
 
@@ -15,5 +21,10 @@ class CallBackViewSet(ViewSet):
 
 		if not (name and phone):
 			return Response({'message':'Invalid data'}, status= 400)
+
+		msg_html = render_to_string('callback/email.html', {'phone': phone, 'name' : name})
+
+		is_sended = send_mail('Вас просять передзвонити', msg_html, 'admin@yakuzalviv.com', ['yakuzalviv@gmail.com', 'oneostap@gmail.com'], html_message= msg_html,)
+
 		CallBack(name= name, phone= phone).save()
 		return Response({'message':'success'})
--- order/views.py
+++ order/views.py
@@ -22,7 +22,6 @@ from django.core.mail import send_mail
 from django.template.loader import render_to_string
 
 from liqpay.liqpay3 import LiqPay
-from django.conf import settings
 
 from subscribers.models import Subscriber
 
