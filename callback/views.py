8b8a2168ff6d28f6af952e7c5696d79eaac43b53
Ostap Hyba <oneostap@gmail.com>
2018-04-12 16:06:09 +0300

    add callback email send

--- callback/views.py
+++ callback/views.py
@@ -3,6 +3,12 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from .models import CallBack
 
+from django.conf import settings
+
+from django.core.mail import send_mail
+
+from django.template.loader import render_to_string
+
 
 class CallBackViewSet(ViewSet):
 
@@ -11,9 +17,18 @@ class CallBackViewSet(ViewSet):
 		name = request.data['name'] if 'name' in request.data else None
 		phone = request.data['cell'] if 'cell' in request.data else None
 
-		print(request.data)
-
 		if not (name and phone):
 			return Response({'message':'Invalid data'}, status= 400)
+
+		msg_html = render_to_string('callback/email.html', {'name': name, 'phone' : phone})
+
+		is_sended = send_mail(
+			'Вас просять передзвонити', 
+			msg_html, 
+			'admin@yakuzalviv.com', 
+			['yakuzalviv@gmail.com', 'oneostap@gmail.com'], 
+			html_message= msg_html,
+			)
+		
 		CallBack(name= name, phone= phone).save()
 		return Response({'message':'success'})
