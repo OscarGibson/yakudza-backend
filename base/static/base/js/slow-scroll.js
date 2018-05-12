e1cc85737a5828c02595a3188deb8c073acdb061
ostap <oneostap@gmail.com>
2018-05-12 14:21:52 +0300

    remove console.log

--- base/static/base/js/slow-scroll.js
+++ base/static/base/js/slow-scroll.js
@@ -2,7 +2,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
 
-        console.log(this.getAttribute('href'));
+        // console.log(this.getAttribute('href'));
 
         document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth',
